7f4c940cbbdfffc275ac5ea5a14b628164143916
saifchaudhry <schaudhry722@gmail.com>
2025-12-01 17:47:48 +0500

    Updated Supabse config

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,5 +1,5 @@
 // Supabase configuration
 export const SUPABASE_CONFIG = {
-  url: process.env.REACT_APP_SUPABASE_URL || 'https://vmbiwdqxypltrmnvitpi.supabase.co',
-  anonKey: process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtYml3ZHF4eXBsdHJtbnZpdHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NTE1NjgsImV4cCI6MjA3NzIyNzU2OH0.c0gVfoDuPcFDkyqznMK9sjZV1wtkHPvGyLK78kxWQ4c',
+  url: process.env.REACT_APP_SUPABASE_URL || 'https://wryeixphpqbvhvqcsuom.supabase.co',
+  anonKey: process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyeWVpeHBocHFidmh2cWNzdW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MjMzODEsImV4cCI6MjA4MDA5OTM4MX0.edtU-oYLGWT3CNzJrOoLlDotSsx6eN2awdzbCumjbqk',
 };
